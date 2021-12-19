e82520bf35e6825ed90af0855ed6949a41a92e3f
Rick Dennison <rdennison12@gmmail.com>
2021-12-19 08:43:47 -0500

    Lists, functions, and constants

--- README.md
+++ README.md
@@ -73,3 +73,4 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/t
 
 * Project created and added to version control
 * Basics of React to JavaScript
+* Lists, functions, and constants 
--- src/App.js
+++ src/App.js
@@ -1,22 +1,60 @@
 import * as React from 'react'
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  const welcome = {
-    greeting: 'Hey',
-    title: 'React'
-  };
-  
-  return ( 
-    <div>
-        <h1>{welcome.greeting} {welcome.title}</h1>
+// const welcome = {
+//     greeting: 'Hey',
+//     title: 'React'
+// };
+// function getTitle(title) {
+//     return title;
+// }
+const list = [
+    {
+        title: 'React',
+        url: 'https://reactjs.org',
+        author: 'Jordan Walke',
+        num_comments: 3,
+        points: 4,
+        objectID: 0
+    },
+    {
+        title: 'Redux',
+        url: 'https://redux.js.org',
+        author: 'Dan Abramov, Andrew Clark',
+        num_comments: 2,
+        points: 5,
+        objectID: 1
+    }
+];
 
-        <label htmlFor='search'>Search: </label>
-        <input id='search' type='text' />
-    </div>
-    
-  );
+function App() {
+
+
+    return (
+        <div>
+            <h1>My Hacker Stories</h1>
+
+            <label htmlFor='search'>Search: </label>
+            <input id='search' type='text'/>
+            <hr/>
+            <ul>
+                {list.map(function (item) {
+                    return (
+                        <li key={item.objectID}>
+                            <span>
+                                <a href={item.url}>{item.title}</a>
+                            </span>
+                            <span>{item.author}</span>
+                            <span>{item.num_comments}</span>
+                            <span>{item.points}</span>
+                        </li>
+                    );
+                })}
+            </ul>
+        </div>
+
+    );
 }
 
 export default App;
